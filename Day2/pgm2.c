cc60ff258d5217c781a350d6a018f393f327581f
varshini0605 <varshini.g605@gmail.com>
2024-11-12 10:08:19 +0530

    programs  are added

---
+++ Day2/pgm2.c
@@ -0,0 +1,37 @@
+// program that reverses the elements of an array in place using pointers.
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int *start = arr;
+    int *end = arr + n - 1;
+
+    while (start < end) 
+    {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
